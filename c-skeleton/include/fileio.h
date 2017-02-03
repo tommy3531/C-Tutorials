37e0b791d3ea38553ac52f6445e45af8eb644b53
tommarler <thomas.marler@hotmail.com>
2017-02-03 06:47:17 -0600

    Added function to check if array is empty

--- c-skeleton/include/fileio.h
+++ c-skeleton/include/fileio.h
@@ -2,42 +2,85 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#define BSIZE 16384
+#define MAX_CUSTOMERS 100
 
-void fileCopy(FILE *readFrom, FILE *readTo)
-{
+typedef struct Customer {
+    char name[20];
+    int number;
+}Customer;
 
+//int index = 0;
 
-	char buf[BSIZE];
-	int count;
+int readFromFile(Customer customer[], int i, FILE *fp){
 
+    if(fp == NULL){
+        printf("Error\n");
+        return -1;
+    }
 
-
-	while((count = fread(buf, 1, BSIZE, readFrom)) > 0)
-	{
-		//printf("File Opended");
-		fwrite(buf, 1, count, readTo);
-	}
-	printf("Data Read from read.txt to write.txt, Closing!!!\n");
-
-	fclose(readFrom);
-	fclose(readTo);
-
-	printf("Both files are closed\n");
-
+    fscanf(fp, "%s \t %d\n", customer[i].name, &customer[i].number);
+    return 1;
 }
+
+int left(int index) {
+    int leftNode = 2 * index;
+    printf("This is the leftnode %d", leftNode);
+    return leftNode;
+}
+
+int right(int index){
+    int rightNode = 2 * index + 1;
+    printf("This is the rightNode %d", rightNode);
+    return rightNode;
+}
+void checkIfArrayHasRoom(Customer customer[], int index) {
+    while(customer[index].number != EOF){
+        if(index < MAX_CUSTOMERS) {
+            customer[index++] = customer[index].number;
+        }
+
+    }
+}
+
+
+
+
+
+//void fileCopy(FILE *readFrom, FILE *readTo)
+//{
+//
+//
+//    char buf[BSIZE];
+//    int count;
+//
+//
+//
+//    while((count = fread(buf, 1, BSIZE, readFrom)) > 0)
+//    {
+//        //printf("File Opended");
+//        fwrite(buf, 1, count, readTo);
+//    }
+//    printf("Data Read from read.txt to write.txt, Closing!!!\n");
+//
+//    fclose(readFrom);
+//    fclose(readTo);
+//
+//    printf("Both files are closed\n");
+//
+//}
+
 
 /*
  *
  *  Writes a string of characters to file
  */
 
-void fileFputs(FILE *ptr)
-{
-	if(!ptr)
-	{
-		printf("The file cannot be opened");
-		exit(1);
-	}
-	fputs("This is written to file with fputs new thing\n", ptr);
-} 
+//void fileFputs(FILE *ptr)
+//{
+//    if(!ptr)
+//    {
+//        printf("The file cannot be opened");
+//        exit(1);
+//    }
+//    fputs("This is written to file with fputs new thing\n", ptr);
+//}
--- c-skeleton/src/fileio.c
+++ c-skeleton/src/fileio.c
@@ -2,21 +2,44 @@
 #include <stdbool.h>
 #include "../include/fileio.h"
 
-typedef struct Customer {
-	char name[20];
-	int number;
-}Customer;
+#define MAX_CUSTOMERS 100
+#define NAME_MAX_LENGHT 20
+
+//int readFromFile(struct Customer[], int, FILE *);
+
+
 
 int main(int argc, const char *argv[])
 {
-	Customer customer;
+	Customer customer[MAX_CUSTOMERS];
+    int index = 0;
+    int size = 0;
 	FILE *fptr;
 
 	if((fptr = fopen("../include/data/customer.txt", "r")) == NULL) {
 		printf("Error! Can not open file\n");
 		exit(1);
 	}
-	while(fscanf(fptr, "%s %d\n", customer.name, &customer.number) != EOF) {
-		printf("%s %d\n", customer.name, customer.number);
-	}
+
+    while(!feof(fptr)) {
+        readFromFile(customer, index, fptr);
+        checkIfArrayHasRoom(customer, index);
+        index++;
+    }
+
+    size = index;
+    printf("The size is %d\n", size);
+
+    for(index = 0; index < size; index++){
+        printf("%s %d\n", customer[index].name, customer[index].number);
+    }
+
+    fclose(fptr);
 }
+
+
+
+
+//while(fscanf(fptr, "%s %d\n", customer.name, &customer.number) != EOF) {
+////		printf("%s %d\n", customer.name, customer.number);
+////	}
