4f0c5075fab6bc6d041166a096b4641564015312
tommarler <thomas.marler@hotmail.com>
2017-02-05 13:01:22 -0600

    Still working on trying to sort by the struct data member

--- c-skeleton/include/fileio.h
+++ c-skeleton/include/fileio.h
@@ -4,43 +4,51 @@
 #include <unistd.h>
 #define MAX_CUSTOMERS 100
 
-typedef struct Customer {
-    char name[20];
-    int number;
-}Customer;
 
-//int index = 0;
+//void restoreUp(Customer customerStruct[], int);
+//
+//
+//
+//Customer customer[MAX_CUSTOMERS];
 
-int readFromFile(Customer customer[], int i, FILE *fp){
+//int left(int index) {
+//    int leftNode = 2 * index;
+//    printf("This is the leftnode %d", leftNode);
+//    return leftNode;
+//}
+//
+//int right(int index){
+//    int rightNode = 2 * index + 1;
+//    printf("This is the rightNode %d", rightNode);
+//    return rightNode;
+//}
+//void checkIfArrayHasRoom(Customer customer[], int index) {
+//    while(customer[index].number != EOF){
+//        if(index < MAX_CUSTOMERS) {
+//            customer[index++] = customer[index].number;
+//        }
+//
+//    }
+//}
 
-    if(fp == NULL){
-        printf("Error\n");
-        return -1;
-    }
+//void insert(int *parsedNumberValueFromFile,  char *parsedNameFromFile, int heapInsertionPosition){
+//
+//
+//    // Increase heapSize becasue we are adding one item to heap
+//    heapInsertionPosition++;
+//
+//    // Insert value into the struct at the given position
+//    customer[heapInsertionPosition].number = *parsedNumberValueFromFile;
+//
+//    // Insert Char into the struct at the given position
+//    customer[heapInsertionPosition].name = parsedNameFromFile;
+//
+//    // Pass the searchCustomerStruct[] with the heapInsertionPosition to restoreUpFunction
+//    restoreUp(customer, heapInsertionPosition);
+//
+//}
 
-    fscanf(fp, "%s \t %d\n", customer[i].name, &customer[i].number);
-    return 1;
-}
 
-int left(int index) {
-    int leftNode = 2 * index;
-    printf("This is the leftnode %d", leftNode);
-    return leftNode;
-}
-
-int right(int index){
-    int rightNode = 2 * index + 1;
-    printf("This is the rightNode %d", rightNode);
-    return rightNode;
-}
-void checkIfArrayHasRoom(Customer customer[], int index) {
-    while(customer[index].number != EOF){
-        if(index < MAX_CUSTOMERS) {
-            customer[index++] = customer[index].number;
-        }
-
-    }
-}
 
 
 
--- c-skeleton/src/fileio.c
+++ c-skeleton/src/fileio.c
@@ -3,11 +3,13 @@
 #include "../include/fileio.h"
 
 #define MAX_CUSTOMERS 100
-#define NAME_MAX_LENGHT 20
+typedef struct Customer {
+    char name[20];
+    int number;
+}Customer;
 
-//int readFromFile(struct Customer[], int, FILE *);
-
-
+void restoreUp(struct Customer customerStruct[], int);
+//struct Customer heapify(struct Customer customerStruct[], int);
 
 int main(int argc, const char *argv[])
 {
@@ -21,21 +23,104 @@ int main(int argc, const char *argv[])
 		exit(1);
 	}
 
-    while(!feof(fptr)) {
-        readFromFile(customer, index, fptr);
-        checkIfArrayHasRoom(customer, index);
+    while(fscanf(fptr, "%s %d", customer[index].name, &customer[index].number) != EOF) {
+        //heapify(customer, index);
         index++;
     }
 
     size = index;
+
+     //Text file data has been read into Customer struct
+
+
+
     printf("The size is %d\n", size);
 
     for(index = 0; index < size; index++){
-        printf("%s %d\n", customer[index].name, customer[index].number);
+        printf("This is the index value: %d\n", index);
+        printf("This is the customer name: %s\n", customer[index].name);
+        printf("This is the customer number: %d\n\n", customer[index].number);
     }
+
+
 
     fclose(fptr);
 }
+
+//void heapSort(struct Customer customerStruct[], int index){
+//
+//    struct Customer customer[] = {{{0,0}}};
+//    customer[index].number = heapify(customerStruct, index);
+//    printf("%d", customer[index].number);
+//
+//        //readFromFile(customer, index, fptr);
+//        //checkIfArrayHasRoom(customer, index);
+//        //printf("%s %d\n", customer[index].name, customer[index].number);
+//}
+//
+//
+//struct Customer[] heapify(struct Customer customerStruct[], int index){
+//
+//    int item,i,j,k;
+//    for(k=1 ; k<index ; k++)
+//    {
+//        item = customerStruct[k].number;
+//        i = k;
+//        j = (i-1)/2;
+//
+//        while( (i>0) && (item>customerStruct[j].number) )
+//        {
+//            customerStruct[i].number = customerStruct[j].number;
+//            i = j;
+//            j = (i-1)/2;
+//        }
+//        customerStruct[i].number = item;
+//        printf("%d\n", customerStruct[item].number);
+//    }
+//    return customerStruct;
+//    //printf("%s", customerStruct[])
+//}
+//void restoreUp(struct Customer customerStruct[], int keyThatValiatesTheHeapOrder){
+//
+//    // Key that has to be more up in the order
+//    int key = 0;
+//
+//    // Parnet key
+//    int parent = 0;
+//
+//    // Get the key that is in valilation of the heap order
+//    key = customerStruct[keyThatValiatesTheHeapOrder].number;
+//
+//    // Calculate the parent keys position within the array
+//    parent = keyThatValiatesTheHeapOrder/2;
+//
+//    while(customerStruct[parent].number > key){
+//        customerStruct[keyThatValiatesTheHeapOrder].number = customerStruct[parent].number;
+//        keyThatValiatesTheHeapOrder = parent;
+//        parent = keyThatValiatesTheHeapOrder/2;
+//    }
+//    customerStruct[keyThatValiatesTheHeapOrder].number = key;
+//
+//    for(index = 0; index < size; index++){
+//        printf("This is the index value : %d\n", index);
+//        printf("This is the customer name: %s\n", customer[index].name);
+//        printf("This is the customer number: %d\n\n", customer[index].number);
+//        //readFromFile(customer, index, fptr);
+//        //checkIfArrayHasRoom(customer, index);
+//        //printf("%s %d\n", customer[index].name, customer[index].number);
+//    }
+//
+//    printf("This is the sorted struct array\n");
+//    for(int i = 0; i < 100; i++){
+//        printf("Sorting by this number: %d\n\n", customerStruct[i].number);
+////        printf("Customer Name: %s\n", customerStruct[i].name);
+////        printf("Index Value: %d\n", i);
+//
+//
+//    }
+//
+//
+//}
 
 
 
