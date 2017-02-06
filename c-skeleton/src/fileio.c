c248a8d976bd4bc308f3526a62a4ac8a562fa0e1
tommarler <thomas.marler@hotmail.com>
2017-02-05 22:12:05 -0600

    Able to sort struct array by data member number, reads data from the

--- c-skeleton/src/fileio.c
+++ c-skeleton/src/fileio.c
@@ -3,13 +3,17 @@
 #include "../include/fileio.h"
 
 #define MAX_CUSTOMERS 100
-typedef struct Customer {
+typedef struct customer {
     char name[20];
     int number;
 }Customer;
 
-void restoreUp(struct Customer customerStruct[], int);
-//struct Customer heapify(struct Customer customerStruct[], int);
+void buildHeap(struct customer customer[], int);
+void printHeapTree(struct customer customer[]);
+void heapify(struct customer searchCustomer[], int, int);
+void restoreUp(struct customer customer[], int);
+void sort(struct customer customer[], int);
+void swap(struct customer customer[], int, int);
 
 int main(int argc, const char *argv[])
 {
@@ -18,24 +22,19 @@ int main(int argc, const char *argv[])
     int size = 0;
 	FILE *fptr;
 
-	if((fptr = fopen("../include/data/customer.txt", "r")) == NULL) {
+	if((fptr = fopen("../include/data/text.txt", "r")) == NULL) {
 		printf("Error! Can not open file\n");
 		exit(1);
 	}
 
     while(fscanf(fptr, "%s %d", customer[index].name, &customer[index].number) != EOF) {
-        //heapify(customer, index);
         index++;
     }
 
     size = index;
-
-     //Text file data has been read into Customer struct
-
-
-
-    printf("The size is %d\n", size);
-
+    sort(customer, size);
+    printHeapTree(customer);
+    printf("This is the sort heap represents the diagram above\n");
     for(index = 0; index < size; index++){
         printf("This is the index value: %d\n", index);
         printf("This is the customer name: %s\n", customer[index].name);
@@ -43,10 +42,86 @@ int main(int argc, const char *argv[])
     }
 
 
-
     fclose(fptr);
 }
 
+void sort(struct customer searchCustomer[], int numberOfStructElements){
+    int total = numberOfStructElements - 1;
+    printf("This is total in sort function: %d\n", total);
+
+    for(int i = total / 2; i >= 0; i--){
+        printf("This is i inside the for loop within sort: %d\n", i);
+        heapify(searchCustomer, i, total);
+    }
+
+    //for(int i = total; i > 0; i--)
+}
+
+void heapify(struct customer searchCustomer[], int index, int total){
+    printf("Heapify [%d] = %d\n", index, searchCustomer[index].number);
+    int left = index * 2;
+    int right = left + 1;
+    int parent = index;
+
+    if(left <= total && searchCustomer[left].number > searchCustomer[parent].number){
+        parent = left;
+    }
+
+    if(right <= total && searchCustomer[right].number > searchCustomer[parent].number){
+        parent = right;
+    }
+
+    if(parent != index){
+        swap(searchCustomer, index, parent);
+        heapify(searchCustomer, parent, total);
+    }
+
+}
+
+void swap(struct customer searchCustomer[], int index, int parent){
+    do {
+        int temp = searchCustomer[index].number;
+        searchCustomer[index].number = searchCustomer[parent].number;
+        searchCustomer[parent].number = temp;
+    } while (0);
+}
+
+void buildHeap(struct customer customer[], int index){
+    printf("Building the heap\n");
+    for(int i = 2; i <= index; i++){
+        restoreUp(customer, i);
+    }
+}
+
+void restoreUp(struct customer customer[], int index){
+    int key = customer[index].number;
+    int parent = index / 2;
+
+    while(customer[parent].number < key){
+        customer[index].number = customer[parent].number;
+        index = parent;
+        parent = index / 2;
+    }
+
+    customer[index].number = key;
+}
+
+void printHeapTree(struct customer customer[]){
+
+printf("                                    %s %d\n", "index->0", customer[0].number);
+printf("                  %s %d                                %d %s\n\n", "index->1", customer[1].number, customer[2].number, "2<-index");
+printf("          %s %d                   %d %s                    %s %d                   %d %s\n\n","index->3", customer[3].number, customer[4].number, "4<-index", "index->5", customer[5].number, customer[6].number, "6<-index");
+printf("%s %d       %s %d     %s %d          %s % d          %s %d       %s %d           %s %d \n", "index->7", customer[7].number,"index->8", customer[8].number,
+                                                                                     "index->9", customer[9].number,"index->10", customer[10].number,
+                                                                                     "index->11", customer[11].number,"index->12", customer[12].number,
+                                                                                     "index->13", customer[13].number);
+
+}
+
+
+
+
+////////////////// Extra Code /////////////////////////////////////////////////////////////////////////////////////////
 //void heapSort(struct Customer customerStruct[], int index){
 //
 //    struct Customer customer[] = {{{0,0}}};
@@ -121,6 +196,21 @@ int main(int argc, const char *argv[])
 //
 //
 //}
+
+//
+//     //Text file data has been read into Customer struct
+//
+//
+//
+//    printf("The size is %d\n", size);
+//
+//    for(index = 0; index < size; index++){
+//        printf("This is the index value: %d\n", index);
+//        printf("This is the customer name: %s\n", customer[index].name);
+//        printf("This is the customer number: %d\n\n", customer[index].number);
+//    }
+
+
 
 
 
