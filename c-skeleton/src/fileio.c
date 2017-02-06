02c328e2780658a0a9736c69b3e9678357e288e1
tommarler <thomas.marler@hotmail.com>
2017-02-06 14:23:00 -0600

    Sorts by alpha and also sorts by int

--- c-skeleton/src/fileio.c
+++ c-skeleton/src/fileio.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <dbg.h>
 #include "../include/fileio.h"
 
 #define MAX_CUSTOMERS 100
@@ -11,10 +12,14 @@ typedef struct customer {
 void buildHeap(struct customer customer[], int);
 void printHeapTree(struct customer customer[]);
 void heapify(struct customer searchCustomer[], int, int);
-void restoreUp(struct customer customer[], int);
 void sort(struct customer customer[], int);
 void swap(struct customer customer[], int, int);
 void printData(struct customer customer[], int);
+void sortAlpha(struct customer searchCustomer[], int);
+void heapifyAlpha(struct customer searchCustomer[], int, int);
+void swapAlpha(struct customer searchCustomer[], int, int);
+void printDataAlpha(struct customer customer[], int);
+
 
 int main(int argc, const char *argv[])
 {
@@ -34,42 +39,145 @@ int main(int argc, const char *argv[])
 
     size = index;
     printf("This is the size %d\n", size);
+
+    // This calls sorts by alpha
+    sortAlpha(customer, size);
+
+    // This display is alpha
+    printDataAlpha(customer, size);
+
+    // This calls sort by int
     sort(customer, size);
-    printHeapTree(customer);
+
+    // This sorts by int
     printData(customer, size);
 
     fclose(fptr);
 }
 
+void sortAlpha(struct customer searchCustomer[], int numberOfStructElements){
+    int total = numberOfStructElements - 1;
+
+    // This builds the heap
+    for(int i = total / 2; i >= 0; i--){
+        heapifyAlpha(searchCustomer, i, total);
+    }
+
+    // Go through the heap one element at a time
+    for(int i = total; i > 0; i--){
+
+        // Move parent to end
+        swapAlpha(searchCustomer, 0, i);
+
+        // Reduce the heapsize
+        total--;
+
+        // Since I reduced the heapsize I need to call heapify
+        heapifyAlpha(searchCustomer, 0, total);
+    }
+
+}
+
+
+void heapifyAlpha(struct customer searchCustomer[], int index, int total){
+
+    int largest = index;
+    int left = 2 * index;
+    int right = 2 * index + 1;
+
+    // If the left child is larger than the parent node
+    if(left <= total && searchCustomer[left].name > searchCustomer[largest].name){
+        // Make the left the largest node
+        largest = left;
+
+    }
+
+    // If the right child is larger than the parent node
+    if(right <= total && searchCustomer[right].name > searchCustomer[largest].name){
+
+        // Make the right node the largest
+        largest = right;
+    }
+
+    // If the largest node is not the parent
+    if(largest != index){
+        swapAlpha(searchCustomer, largest, total);
+        heapifyAlpha(searchCustomer, largest, total);
+    }
+
+}
+
+void swapAlpha(struct customer searchCustomer[], int index, int largest){
+
+    // If the result is > 0 swap index is the largest
+    // If the result is < 0 the largest is largest
+    // If the result is == 0 they are equal
+    int result = strcmp(searchCustomer[index].name, searchCustomer[largest].name);
+    if(result > 0){
+        struct customer temp = searchCustomer[index];
+        searchCustomer[index] = searchCustomer[largest];
+        searchCustomer[largest] = temp;
+    }
+}
+
+void printDataAlpha(struct customer customer[], int size){
+
+    printf("\nWe have the following customers (sorted by their names): \n\n");
+    for (int i = 0; i <= size; i++) {
+        printf("%10s   %d\n", customer[i].name, customer[i].number);
+    }
+    printf("\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////// This work dont mess with it ///////////////////////////////////////////////////////////////////
 void sort(struct customer searchCustomer[], int numberOfStructElements){
     int total = numberOfStructElements - 1;
-    printf("This is total in sort function: %d\n", total);
 
+    // Build the heap
     for(int i = total / 2; i >= 0; i--){
-        printf("This is i inside the for loop within sort: %d\n", i);
         heapify(searchCustomer, i, total);
     }
+
+    // Extract element one by one from heap
     for(int i = total; i > 0; i--){
+
+        // Move top root element to end element
         swap(searchCustomer, 0, i);
+
+        // Reduce heap size
         total--;
+
+        // Call heapify on reduced heap
         heapify(searchCustomer, 0, total);
     }
 
 }
 
 void heapify(struct customer searchCustomer[], int index, int total){
-    printf("Heapify [%d] = %d\n", index, searchCustomer[index].number);
+    //printf("Heapify [%d] = %d\n", index, searchCustomer[index].number);
 
     int largest = index;
     int left = 2 * index;
     int right = 2 * index + 1;
 
-    if(left <= total && searchCustomer[left].number < searchCustomer[largest].number){
+    if(left <= total && searchCustomer[left].number > searchCustomer[largest].number){
         largest = left;
 
     }
 
-    if(right <= total && searchCustomer[right].number < searchCustomer[largest].number){
+    if(right <= total && searchCustomer[right].number > searchCustomer[largest].number){
         largest = right;
     }
 
@@ -101,8 +209,8 @@ printf("%s %d       %s %d    %s %d       %s % d      %s %d  %s %d   %s %d \n    
 }
 
 void printData(struct customer customer[], int size){
+    printf("Their service order should be\n\n");
     for (int i = 0; i <= size; i++) {
-        //printf("Finally got it to sort\n");
         printf("%10s   %d\n", customer[i].name, customer[i].number);
     }
 }
@@ -111,6 +219,31 @@ void printData(struct customer customer[], int size){
 
 
 ////////////////// Extra Code /////////////////////////////////////////////////////////////////////////////////////////
+//resultLeft = strcmp(searchCustomer[left].name, searchCustomer[largest].name);
+//printf("This is the index value: %d\n ",index);
+//printf("Comparing Left %s with %s\n", searchCustomer[left].name, searchCustomer[largest].name);
+//printf("This is the Leftresult: %3d\n\n", resultLeft);
+//
+//
+//resultRight = strcmp(searchCustomer[right].name, searchCustomer[largest].name);
+//printf("Comparing Right %s with %s\n", searchCustomer[right].name, searchCustomer[largest].name);
+//printf("This is the Rightresult: %d\n\n", resultRight);
+
+//    if(result < 0){
+//        printf("Left %s < Largest %s\n", searchCustomer[left].name, searchCustomer[largest].name);
+//    }
+//
+//    if(result > 0){
+//        printf("Left %s > Largest %s\n", searchCustomer[left].name, searchCustomer[largest].name);
+//    }
+//
+//    if(result == 0){
+//        printf("Left %s == Largest %s\n", searchCustomer[left].name, searchCustomer[largest].name);
+//    }
+
+//printf("%s < %s\n", searchCustomer[left].name, searchCustomer[largest].name);
+//printf("%d = %d\n", largest, left);
+//printf("%s %s\n", searchCustomer[left].name, searchCustomer[largest].name);
 //void heapSort(struct Customer customerStruct[], int index){
 //
 //    struct Customer customer[] = {{{0,0}}};
