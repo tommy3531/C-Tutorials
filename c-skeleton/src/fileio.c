0fb1296595628eaad8a019e6921e1011148ce30f
tommarler <thomas.marler@hotmail.com>
2017-02-06 22:16:33 -0600

    Finished, can sort by alpha and also sort numerical to complie gcc -o main.c main ./main test.txt > output-file.txt nano output-file.txt

--- c-skeleton/src/fileio.c
+++ c-skeleton/src/fileio.c
@@ -20,6 +20,7 @@ void heapifyAlpha(struct customer searchCustomer[], int, int);
 void swapAlpha(struct customer searchCustomer[], int, int);
 void printDataAlpha(struct customer customer[], int);
 int stringCompare(struct customer customer[], int, int);
+int printMin(struct customer customer[], int);
 
 
 int main(int argc, const char *argv[])
@@ -38,8 +39,8 @@ int main(int argc, const char *argv[])
         index++;
     }
 
+    // This is the number of struct elements
     size = index;
-    printf("This is the size %d\n", size);
 
     // This calls sorts by alpha
     sortAlpha(customer, size);
@@ -53,13 +54,15 @@ int main(int argc, const char *argv[])
     // This sorts by int
     printData(customer, size);
 
-    int stringCompareValue = stringCompare(customer, 0, 1);
-    printf("%d", stringCompareValue);
+    // This prints the total of number
+    int minData = printMin(customer, size);
+    printf("The total waiting time of all customers will be : \n %d", minData);
 
     fclose(fptr);
 }
 
 void sortAlpha(struct customer searchCustomer[], int numberOfStructElements){
+
     int total = numberOfStructElements - 1;
 
     // This builds the heap
@@ -79,9 +82,7 @@ void sortAlpha(struct customer searchCustomer[], int numberOfStructElements){
         // Since I reduced the heapsize I need to call heapify
         heapifyAlpha(searchCustomer, 0, total);
     }
-
 }
-
 
 void heapifyAlpha(struct customer searchCustomer[], int index, int total){
 
@@ -91,6 +92,7 @@ void heapifyAlpha(struct customer searchCustomer[], int index, int total){
 
     // If the left child is larger than the parent node
     if(left <= total && searchCustomer[left].name > searchCustomer[largest].name){
+
         // Make the left the largest node
         largest = left;
 
@@ -108,15 +110,12 @@ void heapifyAlpha(struct customer searchCustomer[], int index, int total){
         swapAlpha(searchCustomer, largest, total);
         heapifyAlpha(searchCustomer, largest, total);
     }
-
 }
 
 int stringCompare(struct customer customer[], int index, int largest){
 
     int count;
-    printf("%s %s\n", customer[index].name, customer[largest].name);
     count = (*customer[index].name - *customer[largest].name);
-    printf("This is the result value in stringCompare: %d\n", count);
     return count;
 }
 
@@ -126,8 +125,6 @@ void swapAlpha(struct customer searchCustomer[], int index, int largest){
     // If the result is < 0 the largest is largest
     // If the result is == 0 they are equal
     int result = stringCompare(searchCustomer, index, largest);
-    printf("This is the result in swapAlpha: %d\n", result);
-    //int result = strcmp(searchCustomer[index].name, searchCustomer[largest].name);
     if(result > 0){
         struct customer temp = searchCustomer[index];
         searchCustomer[index] = searchCustomer[largest];
@@ -164,10 +161,10 @@ void sort(struct customer searchCustomer[], int numberOfStructElements){
         // Call heapify on reduced heap
         heapify(searchCustomer, 0, total);
     }
-
 }
 
 void heapify(struct customer searchCustomer[], int index, int total){
+
 
     int largest = index;
     int left = 2 * index;
@@ -197,7 +194,7 @@ void heapify(struct customer searchCustomer[], int index, int total){
 
 void swap(struct customer searchCustomer[], int index, int largest){
 
-    // Keep on doing th swap until o
+    // Keep on doing th swap
     do {
         struct customer temp = searchCustomer[index];
         searchCustomer[index] = searchCustomer[largest];
@@ -205,16 +202,17 @@ void swap(struct customer searchCustomer[], int index, int largest){
     } while (0);
 }
 
-void printHeapTree(struct customer customer[]){
+int printMin(struct customer customer[], int size){
 
-printf("                                    %s %d\n", "index->0", customer[0].number);
-printf("                  %s %d                            %s %d\n\n", "index->1", customer[1].number, "index->2", customer[2].number);
-printf("          %s %d             %s %d                %s %d               %s %d\n\n","index->3", customer[3].number, "index->4", customer[4].number,"index->5", customer[5].number, "index->6", customer[6].number);
-printf("%s %d       %s %d    %s %d       %s % d      %s %d  %s %d   %s %d \n    %s %d\n\n", "index->7", customer[7].number,"index->8", customer[8].number,
-                                                                                     "index->9", customer[9].number,"index->10", customer[10].number,
-                                                                                     "index->11", customer[11].number,"index->12", customer[12].number,
-                                                                                     "index->13", customer[13].number, "index->14", customer[14].number);
-
+    int count = size;
+    int temp = 0;
+    int result = 0;
+    for(int i = 0; i < size; i++){
+        temp = customer[i].number;
+        result += temp * count;
+        count--;
+    }
+    return result;
 }
 
 void printData(struct customer customer[], int size){
